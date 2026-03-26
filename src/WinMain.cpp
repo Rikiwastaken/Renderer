a2de293fcfbd3bb7ebdf64d0ebf0255e331de1dc
arnaudkrafft <arnaud.krafft@telecomnancy.net>
2026-03-26 15:05:38 +0100

    Windows Message functionnal

--- src/WinMain.cpp
+++ src/WinMain.cpp
@@ -8,14 +8,24 @@ using namespace std;
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) // Window procedure to handle messages for the window
 {
-    static WindowsMessageMap messageMap;                                  // Create an instance of the WindowsMessageMap to convert messages to strings
-    OutputDebugStringA((messageMap(msg, lParam, wParam) + "\n").c_str()); // Output the message information to the debug console
+    static WindowsMessageMap messageMap;                       // Create an instance of the WindowsMessageMap to convert messages to strings
+    std::cout << messageMap(msg, lParam, wParam) << std::endl; // Print the message information to the console
     switch (msg)
     {
-    case WM_CLOSE:
-        PostQuitMessage(7); // Post a quit message to exit the message loop
-        DestroyWindow(hWnd);
+    case WM_CREATE:
+        std::cout << "WM_CREATE" << std::endl;
         break;
+
+    case WM_CLOSE:
+        std::cout << "WM_CLOSE" << std::endl;
+        // Destroy the window — triggers WM_DESTROY
+        DestroyWindow(hWnd);
+        return 0; // Prevent DefWindowProc from destroying again
+
+    case WM_DESTROY:
+        std::cout << "WM_DESTROY" << std::endl;
+        PostQuitMessage(0); // End message loop
+        return 0;
 
     default:
         break;
@@ -25,6 +35,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) // W
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+
+    AllocConsole();
+    freopen("CONOUT$", "w", stdout);
+    std::cout << "Debug output visible in console!" << std::endl;
+
     cout << "Creating window..." << endl;
 
     LPCWSTR pClassName = L"CoolWindowClass";
@@ -67,8 +82,8 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 
     // Message loop
     MSG msg;
-    BOOL result;
-    while (result = GetMessage(&msg, nullptr, 0, 0) > 0) // GetMessage returns > 0 if a message is retrieved, 0 if WM_QUIT is received, and < 0 on error
+    int result;
+    while ((result = GetMessage(&msg, nullptr, 0, 0)) > 0) // GetMessage returns > 0 if a message is retrieved, 0 if WM_QUIT is received, and < 0 on error
     {
         TranslateMessage(&msg); // Translates virtual-key messages into character messages
         DispatchMessage(&msg);  // Dispatches a message to a window procedure
